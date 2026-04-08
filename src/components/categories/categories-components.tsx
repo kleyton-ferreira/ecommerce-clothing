f32b980c1a0b6728a01d1dd4ab6b534487d57bca
Kleyton Ferreira <kleytondev40@gmail.com>
2026-04-08 18:17:09 -0300

    feat: add Categories Component

--- src/components/categories/categories-components.tsx
+++ src/components/categories/categories-components.tsx
@@ -1,20 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 // STYLES
 import './categories-styles.css'
 
 // UTILITZ
 import Category from '../../types/categories.types'
+import env from '../../config/env.config'
 
 const Categories = () => {
   const [categories, setCategories] = useState<Category[]>([])
 
+  console.log({ categories })
+
+  const fetchCategories = async () => {
+    try {
+      const { data } = await axios.get(`${env.apiUrl}/api/category`)
+
+      setCategories(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
   return (
     <div className='categories-container'>
       <div className='categories-content'>
-        {categories.map(() => (
+        {/* {categories.map(() => (
           <CategoriesItem />
-        ))}
+        ))} */}
       </div>
     </div>
   )
