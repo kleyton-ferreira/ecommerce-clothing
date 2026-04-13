2882a1af0c01f16099c8b4f425023c596323d462
Kleyton Ferreira <kleytondev40@gmail.com>
2026-04-13 17:30:38 -0300

    chore: add firebase configuration

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB46Wgt6lYgrfIvPe0eRcPQRYQtu4SMrnQ",
+    authDomain: "ecommerce-clothing-9aa4c.firebaseapp.com",
+    projectId: "ecommerce-clothing-9aa4c",
+    storageBucket: "ecommerce-clothing-9aa4c.firebasestorage.app",
+    messagingSenderId: "288644227985",
+    appId: "1:288644227985:web:2549fd51aa2a92b6b07030"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
