5b160c0a8e2e76ec938587747c6669e21f8877c5
Kleyton Ferreira <kleytondev40@gmail.com>
2026-04-17 17:05:56 -0300

    feat: add UserContext

---
+++ src/context/user-context.tsx
@@ -0,0 +1,47 @@
+import { FunctionComponent, createContext, useState } from 'react'
+import User from '../types/user.types'
+
+interface IUserContext {
+  currentUser: User | null
+  isAuthenticated: boolean
+  loginUser: (user: User) => void
+  logoutUser: () => void
+}
+
+export const UserContext = createContext<IUserContext>({
+  currentUser: null,
+  isAuthenticated: false,
+  loginUser: () => {},
+  logoutUser: () => {}
+})
+
+interface ContextProviderComponents {
+  children: string | any
+}
+
+const UserContextProvider: FunctionComponent<ContextProviderComponents> = ({
+  children
+}) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null)
+
+  // aqui inicialmente começa com o valor false de nao logado depois pode ser true quando estiver logado
+  const isAuthenticated = currentUser !== null
+
+  const loginUser = (user: User) => {
+    setCurrentUser(user)
+  }
+
+  const logoutUser = () => {
+    setCurrentUser(null)
+  }
+
+  return (
+    <UserContext.Provider
+      value={{ currentUser, isAuthenticated, loginUser, logoutUser }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export default UserContextProvider
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,15 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+// CONTEXT
+import UserContextProvider from './context/user-context'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
   </React.StrictMode>
 )
 
